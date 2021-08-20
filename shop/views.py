66161257013d384721e3e833bdc6ceffbf631a3c
Maciej <mac.woj1000@gmail.com>
2021-08-20 22:58:52 +0200

    made register form and sucess message

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from django.contrib import messages
 
 def home(request):
     return render(request, 'shop/home.html')
 
 def signup(request):
-    form = SignUpForm(request.POST)
-    if form.is_valid:
-        pass 
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f" {username} your account was created!")
+            return redirect('home-page') 
     else: 
         form = SignUpForm()
     return render(request, "shop/signup.html", {'form': form})
