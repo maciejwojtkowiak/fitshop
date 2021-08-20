f1721dfadf2bb573165929250bed776b330bec57
Maciej <mac.woj1000@gmail.com>
2021-08-20 20:43:48 +0200

    added a  primitive form, made a better navbar

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 def home(request):
-    return render(request, 'shop/base.html')
+    return render(request, 'shop/home.html')
+
+def signup(request):
+    form = SignUpForm(request.POST)
+    if form.is_valid:
+        pass 
+    else: 
+        form = SignUpForm()
+    return render(request, "shop/signup.html", {'form': form})
