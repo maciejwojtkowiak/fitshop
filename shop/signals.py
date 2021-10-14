c65052c934476b1b9a3dd355048b3c9f18708087
Maciej <mac.woj1000@gmail.com>
2021-10-14 18:22:26 +0200

    auto image deletion when object is deleted

--- shop/signals.py
+++ shop/signals.py
@@ -1,7 +1,7 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.contrib.auth.models import User
-from .models import Profile, Cart
+from .models import Profile, Cart, Item
 
 @receiver(post_save, sender=User)
 def profile_creation(sender, instance, created, **kwargs):
@@ -12,3 +12,7 @@ def profile_creation(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def profile_creation(sender, instance, **kwargs):
         instance.profile.save()
+
+@receiver(pre_delete, sender=Item)
+def mymodel_delete(sender, instance, **kwargs):
+    instance.image.delete(False)
